8080992e250d5f87f9c0183f3520f55a7c495f69
Noah Perez <noah.z.perez.29@gmail.com>
2026-06-11 11:23:43 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -2,7 +2,7 @@
 
 
 
-module tt_um_wokwi_451813217808899073(
+module Noah_Binary_Hex_Decoder(
   input  wire [7:0] ui_in,    // Dedicated inputs
   output wire [7:0] uo_out,    // Dedicated outputs
   input  wire [7:0] uio_in,    // IOs: Input path
